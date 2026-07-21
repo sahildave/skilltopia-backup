e224797ea41b9a63b3b1c8ee71aede291479c121
Sahil Dave <6861255+sahildave@users.noreply.github.com>
2026-07-21 22:04:00 +0800

    minor prettier issue

--- src-tauri/src/provider_scan/scan.rs
+++ src-tauri/src/provider_scan/scan.rs
@@ -492,7 +492,7 @@ pub fn list_projects(root: &Path) -> Result<Vec<ProjectInfo>, String> {
             })
         })
         .collect::<Vec<_>>();
-    projects.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+    projects.sort_by_key(|a| a.name.to_lowercase());
     Ok(projects)
 }
 
